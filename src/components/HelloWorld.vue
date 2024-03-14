1b62d8f0127a907e4df697935449a55f7b226be8
tsutenn <stringkevin@outlook.com>
2024-03-15 06:22:42 +0800

    1

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,58 +1,129 @@
 <template>
-  <div class="hello">
-    <h1>{{ msg }}</h1>
-    <p>
-      For a guide and recipes on how to configure / customize this project,<br>
-      check out the
-      <a href="https://cli.vuejs.org" target="_blank" rel="noopener">vue-cli documentation</a>.
-    </p>
-    <h3>Installed CLI Plugins</h3>
-    <ul>
-      <li><a href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-babel" target="_blank" rel="noopener">babel</a></li>
-      <li><a href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-eslint" target="_blank" rel="noopener">eslint</a></li>
-    </ul>
-    <h3>Essential Links</h3>
-    <ul>
-      <li><a href="https://vuejs.org" target="_blank" rel="noopener">Core Docs</a></li>
-      <li><a href="https://forum.vuejs.org" target="_blank" rel="noopener">Forum</a></li>
-      <li><a href="https://chat.vuejs.org" target="_blank" rel="noopener">Community Chat</a></li>
-      <li><a href="https://twitter.com/vuejs" target="_blank" rel="noopener">Twitter</a></li>
-      <li><a href="https://news.vuejs.org" target="_blank" rel="noopener">News</a></li>
-    </ul>
-    <h3>Ecosystem</h3>
-    <ul>
-      <li><a href="https://router.vuejs.org" target="_blank" rel="noopener">vue-router</a></li>
-      <li><a href="https://vuex.vuejs.org" target="_blank" rel="noopener">vuex</a></li>
-      <li><a href="https://github.com/vuejs/vue-devtools#vue-devtools" target="_blank" rel="noopener">vue-devtools</a></li>
-      <li><a href="https://vue-loader.vuejs.org" target="_blank" rel="noopener">vue-loader</a></li>
-      <li><a href="https://github.com/vuejs/awesome-vue" target="_blank" rel="noopener">awesome-vue</a></li>
-    </ul>
-  </div>
+  <v-container style="width: 100%; height: 100%;">
+
+    <v-navigation-drawer :width="400">
+      <v-list-item title="Geo-Info Graphics" ></v-list-item>
+      <v-divider></v-divider>
+
+      <v-expansion-panels>
+        <v-expansion-panel>
+          <v-expansion-panel-title>
+            item 1
+          </v-expansion-panel-title>
+
+          <v-expansion-panel-text>
+            Some content 1
+            <v-img
+              :src="require('../assets/logo.svg')"
+              class="my-3"
+              contain
+              height="200"
+            />
+          </v-expansion-panel-text>
+        </v-expansion-panel>
+
+        <v-expansion-panel>
+          <v-expansion-panel-title>
+            item 2
+          </v-expansion-panel-title>
+
+          <v-expansion-panel-text>
+            Some content 2
+            <v-img
+              :src="require('../assets/logo.svg')"
+              class="my-3"
+              contain
+              height="200"
+            />
+          </v-expansion-panel-text>
+        </v-expansion-panel>
+
+        <v-expansion-panel>
+          <v-expansion-panel-title>
+            item 3
+          </v-expansion-panel-title>
+
+          <v-expansion-panel-text>
+            Some content 3
+          </v-expansion-panel-text>
+        </v-expansion-panel>
+      </v-expansion-panels>
+    </v-navigation-drawer>
+    
+    <div ref="mapContainer" class="map-container" style="width: 100%; height: 100%;">
+      <svg ref="svg" style="width: 100%; height: 100%;"></svg>
+    </div>
+  </v-container>
 </template>
 
 <script>
+import * as d3 from 'd3';
+
 export default {
   name: 'HelloWorld',
-  props: {
-    msg: String
-  }
-}
-</script>
 
-<!-- Add "scoped" attribute to limit CSS to this component only -->
-<style scoped>
-h3 {
-  margin: 40px 0 0;
+  data: () => ({
+    mapData: {},
+  }),
+
+  mounted() {
+    // 监听窗口大小变化
+    window.addEventListener('resize', this.handleResize);
+
+    // 初次渲染图表
+    this.loadJson('countries.geojson').then(data => {
+      this.mapData = data;
+
+      this.loadJson('country-by-population.json').then(scale => {
+        console.log(scale);
+        this.handleResize(this.mapData);
+      });
+    });
+  },
+
+  beforeUnmount() {
+    // 组件销毁时移除监听器
+    window.removeEventListener('resize', this.handleResize);
+  },
+
+  methods: {
+    handleResize() {
+      this.drawChart(this.mapData);
+    },
+
+    drawChart(data) {
+      const svg = d3.select(this.$refs.svg);
+
+      // 移除现有的 SVG
+      svg.selectAll('*').remove();
+
+      const mapWidth = svg.node().getBoundingClientRect().width;
+      const mapHeight = svg.node().getBoundingClientRect().height;
+
+      const projection = d3.geoMercator().fitSize([mapWidth, mapHeight], data);
+      const path = d3.geoPath().projection(projection);
+
+      svg.selectAll('path')
+        .data(data.features)
+        .enter()
+        .append('path')
+        .attr('d', path)
+        .attr('fill', 'lightgray')
+        .attr('stroke', 'white');
+    },
+
+    loadJson(na) {
+      return new Promise((resolve, reject) => {
+        d3.json(na).then(data => {
+            resolve(data);
+          }).catch(error => {
+            console.error('Error loading GeoJSON data:', error);
+            reject(error);
+          });
+      });
+    },
+
+  },
 }
-ul {
-  list-style-type: none;
-  padding: 0;
-}
-li {
-  display: inline-block;
-  margin: 0 10px;
-}
-a {
-  color: #42b983;
-}
-</style>
+
+</script>
